9c903e9ce109a1d0ee57d449d37117e6692f155d
D17AN <florin.bejenaru@stud.ubbcluj.ro>
2023-04-06 07:51:06 +0300

    Hopefully the final version...

--- Frontend/react-frontend-project-sdi/src/constants.ts
+++ Frontend/react-frontend-project-sdi/src/constants.ts
@@ -1,1 +1,5 @@
-export const BACKEND_API_URL = "http://3.86.251.57"
+const PROD_BACKEND_API_URL = "/api";
+const DEV_BACKEND_API_URL = "http://3.86.251.57/api";
+
+export const BACKEND_API_URL =
+	process.env.NODE_ENV === "development" ? DEV_BACKEND_API_URL : PROD_BACKEND_API_URL
